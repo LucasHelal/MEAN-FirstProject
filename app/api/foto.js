546cdda3a84b25d53e3280ac27ab770333360e23
Lucas Helal <lucasghelal@gmail.com>
2016-04-02 12:41:14 -0300

    remove photo

--- app/api/foto.js
+++ app/api/foto.js
@@ -5,19 +5,28 @@ var fotos = [
   {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg'}
 ];
 
-api.lista = function(req, res){
+api.lista = function(req, res) {
 
   res.json(fotos);
 
 };
 
-api.buscaPorId = function(req, res){
+api.buscaPorId = function(req, res) {
 
   var foto = fotos.find(function(foto) {
       return foto._id = req.params.id;
   });
 
   res.json(foto);
+};
+
+api.removePorId = function(req, res){
+
+  fotos = fotos.filter(function(foto) {
+    return foto._id != req.params.id;
+  })
+  
+  res.sendStatus(204);
 };
 
 module.exports = api;
