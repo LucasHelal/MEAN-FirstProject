a698a5b0d8572bf806d3d72b9fd5231aaffac99e
Lucas Helal <lucasghelal@gmail.com>
2016-04-03 15:07:31 -0300

    Configuração mongoose.

--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express();
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 app.use(express.static('./public')) //configura o middleware para acessar os end dos arq. externos
+app.use(bodyParser.json());
 
 consign({ cwd: 'app'})
   .include('api')
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
-var app = require('./config/express')
+var app = require('./config/express');
+require('./config/database')('localhost/MEAN-FirstProject');
+
 http.createServer(app).listen(3000, function(){
   console.log('Server Start')
 });
