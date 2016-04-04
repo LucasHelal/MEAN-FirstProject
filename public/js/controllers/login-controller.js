f161d5632676864d2b12e48459cbfb25a3dfc07c
Lucas Helal <lucasghelal@gmail.com>
2016-04-04 00:06:35 -0300

    finalizado token para usuario

--- public/js/controllers/login-controller.js
+++ public/js/controllers/login-controller.js
@@ -1,19 +1,18 @@
-angular.module.('alurapic')
-  .controller('LoginController'), function($scope, $http, $location) {
+angular.module.('alurapic').controller('LoginController'), function($scope, $http, $location) {
+
     $scope.usuario = {};
-    $scope.mensagem = '';
 
     $scope.autenticar = function() {
+
       var usuario = $scope.usuario;
 
-      $http.post('/autenticar',
-        {login: usuario.login, senha: usuario.senha})
+      $http.post('/autenticar', {login: usuario.login, senha: usuario.senha})
         .then(function() {
           $location.path('/');
-        }, function(error) {
+        }, function(erro) {
           console.log(error);
           $scope.usuario = {};
           $scope.mensagem = 'Login ou Senha inválidos!';
-        })
+        });
     };
 });
--- public/js/services/token-interceptor.js
+++ public/js/services/token-interceptor.js
@@ -1,7 +1,32 @@
-angular.module('alurapic'){
-  .factory('tokenInterceptor', function() {
+angular.module('alurapic')
+  .factory('tokenInterceptor', function($window, $q, $location) {
     var interceptor = {};
 
+    interceptor.response = function(response) {
+      console.log('Recebi resposta.');
+      var token = response.headers('x-acess-token');
+      if(token) {
+        $window.sessionStorage.token = token;
+        console.log('Token armazenado no navegador.');
+      }
+      return response;
+    };
+
+    interceptor.request = function(config) {
+      config.headers = config.headers || {};
+      if($window.sessionStorage.token) {
+        config.headers['x-acess-token'] = $window.sessionStorage.token;
+        console.log('Adicionando token no header da requisição para o servidor');
+      }
+      return config;
+    };
+
+    interceptor.responseError = function(rejection) {
+      if(rejection != null && rejection.status == 401) {
+        delete $window.sessionStorage.token;
+        $location.path('/login');
+      }
+      return $q.reject(rejection);
+    };
     return interceptor;
   });
-}
